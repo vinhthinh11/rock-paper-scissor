3030aafeffd869461581ab91affc1fc11e691e1e
vinhthinh11 <vinhthinh1993@gmail.com>
2022-11-10 21:41:38 +0700

    Thêm phần hiển thị điểm số cho trang index; them một số chức năng cho Js
    Thêm một ít css, chưa có hiển  thị selection,

--- script.js
+++ script.js
@@ -1,42 +1,94 @@
-function playRound(){
-    let yourChoice = prompt("Chon keo, bao hay bua","keo");
-    yourChoice = yourChoice.toLowerCase();
-    const playerSelection = getPlayerSelection(yourChoice);
-    const computerSelection = getComputerChoise();
-    compare(playerSelection,computerSelection);
+const name1 = document.querySelector('img[data-key="bua"]');
+const name2 = document.querySelector('img[data-key="bao"]');
+const name3 = document.querySelector('img[data-key="keo"]');
+const displayResult = document.querySelector('.display-result');
+const displayPlayerScore = document.querySelector('.player');
+const displayComputerScore = document.querySelector('.computer');
+function update(num1,num2){
+    displayPlayerScore.textContent=num1;
+    displayComputerScore.textContent=num2;
 }
-function game(){
-for(let i =0;i<5;i++){
-    playRound();
+let playerSC =0;
+let computerScore=0;
+let max = getmax(playerSC,computerScore);
+function getmax(num1,num2){
+    return num1>=num2?num1:num2;
 }
+name1.addEventListener('click',()=>getResult(0));
+name2.addEventListener('click',()=>getResult(1));
+name3.addEventListener('click',()=>getResult(2));
+function getResult(selection){
+    let playerSeletion = selection;
+    let computerSelection = Math.floor(Math.random()*3);
+    compare(playerSeletion,computerSelection);
+    
+}
+function isGameOver(){
+    return getmax(playerSC,computerScore)==5;
 }
 
-function getComputerChoise(){
-    return parseInt(Math.random()*3);
-}
-function getPlayerSelection(string){
-    switch (string){
-        case "keo": return 0;
-        case "bao": return 1;
-        case "bua": return 2;
-        default: alert("Ban co biet choi keo bao bua khong ??");
-    }
-}
+
+
+
+    //compare(mychoice,computerSelection);
+
+// function game(){
+//     playRound();
+// }
+
+// function getComputerChoise(){
+//     return parseInt(Math.random()*3).toString();
+// }
 function compare(chon1,chon2){
-    if(chon1==chon2)alert(`ket qua hoa ${chon1==0?"keo":chon1==1?"bao":"bua"}`);
+    if(chon1==chon2){
+        displayResult.textContent="Draw";
+    }
     else {
         if(chon1==0){
-            if(chon2==1)alert("ban thang keo > bao");
-            else alert("ban thua keo < bua");
+            if(chon2==1){
+                displayResult.textContent="You Win";
+                playerSC++;
+                update(playerSC,computerScore);
+                
+                        }
+            else {
+            displayResult.textContent="You Lose";
+            computerScore++;
+            update(playerSC,computerScore);
+                }
+            if(isGameOver())anounceWinner();
         }
         if(chon1==1){
-            if(chon2==0)alert("ban thua bao < keo");
-            else alert("ban thang bao > bua")
+            if(chon2==0){
+                displayResult.textContent="You Lose";
+            computerScore++;
+            update(playerSC,computerScore);
+                        }
+            else{
+                displayResult.textContent="You Win"; 
+            playerSC++;
+            update(playerSC,computerScore);
+                }
+                if(isGameOver())anounceWinner();
         }
         if(chon1==2){
-            if(chon2==0)alert("ban thang bua >keo");
-            else alert("ban thua bua <bao");
+            if(chon2==0){
+                displayResult.textContent="You Win";
+                playerSC++;
+                update(playerSC,computerScore);
+            }
+            else{ 
+                displayResult.textContent="You Lose";
+                computerScore++;
+                update(playerSC,computerScore);
+            }
+            if(isGameOver())anounceWinner();
         }
     }
 }
-game();
+function anounceWinner(){
+    if(playerSC==5)displayResult.textContent="Congratulation!! You win the game";
+            else displayResult.textContent="You Lose!!! Good Luck next time";
+}
+
+// game();
